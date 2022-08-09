6a03fdd57ebe22ca2988084c1fc2b31be3f2f5be
Yohan Jeong <dev.yohanjeong@gmail.com>
2022-08-09 14:48:39 +0900

    0809 test2

--- FarmFarmFarm/src/main/java/net/cart/action/CartAction2.java
+++ FarmFarmFarm/src/main/java/net/cart/action/CartAction2.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CartAction implements Action {
+public class CartAction2 implements Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		int itemid = Integer.parseInt(request.getParameter("id"));
